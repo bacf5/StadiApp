ff175779c62cba7f7932fff19a9b0dd487e5962e
bacf <brunoagustincarusofassa@gmail.com>
2025-07-01 17:47:09 +0200

    Initial commit

---
+++ ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StadiApp
+//
+//  Created by Bruno Agustín Caruso Fassa on 01/07/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StadiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StadiAppApp.swift
+//  StadiApp
+//
+//  Created by Bruno Agustín Caruso Fassa on 01/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct StadiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
