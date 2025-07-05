9c50faf1bc62131f194106453da43bb6b2aa593f
bacf <brunoagustincarusofassa@gmail.com>
2025-07-06 01:09:13 +0200

    Created StadiumDetail view / ScrollView / GeometryReader

--- StadiumDetail.swift
+++ StadiumDetail.swift
@@ -8,11 +8,53 @@
 import SwiftUI
 
 struct StadiumDetail: View {
+    
+    let estadio: Stadium
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        GeometryReader { geo in
+            ScrollView {
+                ZStack(alignment: .bottomTrailing) {
+                    // BG img (Stadium
+                    Image(estadio.imageStadium)
+                        .resizable()
+                        .scaledToFit()
+                
+                    // Club IMG
+                    Image(estadio.imageClub)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: geo.size.width/2.5, height: geo.size.height/4)
+                        .shadow(color: .black, radius: 8)
+                        .offset(y: 55)
+                        .offset(x: -30)
+                        
+                
+                
+                    // Somewehere put the stadium capacity
+                }
+            
+                // Stadium name
+            
+                // Stadium location
+            
+                // Stadium info
+            
+                // Stadium host's matches
+            
+                // Stadium link
+            
+            
+            
+            }
+            
+        }
+        .ignoresSafeArea()
     }
 }
 
 #Preview {
-    StadiumDetail()
+    StadiumDetail(estadio: StadiumClass().allStadiums[2])
+    //        .preferredColorScheme(.dark)
 }
