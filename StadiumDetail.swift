7737182aa198b047552788331ddfe412919eac55
bacf <brunoagustincarusofassa@gmail.com>
2025-07-06 01:04:26 +0200

    Update matches from today

---
+++ StadiumDetail.swift
@@ -0,0 +1,18 @@
+//
+//  StadiumDetail.swift
+//  StadiApp
+//
+//  Created by Bruno Agustín Caruso Fassa on 05/07/2025.
+//
+
+import SwiftUI
+
+struct StadiumDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StadiumDetail()
+}
